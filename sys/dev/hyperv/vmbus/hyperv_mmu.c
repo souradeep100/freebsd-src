e57f4160ea3caf6716ed6fc5514048494d640702
schakrabarti <schakrabarti@microsoft.com>
2024-05-20 22:58:29 +0000

    fixed tlb flushing handling

--- sys/dev/hyperv/vmbus/hyperv_mmu.c
+++ sys/dev/hyperv/vmbus/hyperv_mmu.c
@@ -139,7 +139,7 @@ hv_vm_tlb_flush(pmap_t pmap, vm_offset_t addr1, vm_offset_t addr2, cpuset_t mask
 			return 1;
 	}
 	max_gvas = (PAGE_SIZE - sizeof(*flush)) / sizeof(flush->gva_list[0]);
-	if (op == INVL_OP_PG) {
+	if (addr2 == 0) {
 		flush->flags |= HV_FLUSH_NON_GLOBAL_MAPPINGS_ONLY;
 		status = hypercall_do_md(HVCALL_FLUSH_VIRTUAL_ADDRESS_SPACE, (uint64_t)flush,
 				(uint64_t)NULL);
@@ -155,7 +155,6 @@ hv_vm_tlb_flush(pmap_t pmap, vm_offset_t addr1, vm_offset_t addr2, cpuset_t mask
                                              gva_n, 0, (uint64_t)flush, (uint64_t)NULL);
 
 	}
-	printf("the status of tlb_flush %lu\n", status);
 	return status;		
 do_ex_hypercall:
 	return hv_flush_tlb_others_ex(pmap, addr1, addr2, mask, op, sc);
@@ -203,7 +202,7 @@ hv_flush_tlb_others_ex(pmap_t pmap, vm_offset_t addr1, vm_offset_t addr2, const 
                  sizeof(flush->hv_vp_set.bank_contents[0])) /
                 sizeof(flush->hv_vp_set.bank_contents[0]);
 
-	if (op == INVL_OP_PG) {
+	if (addr2==0) {
                 flush->flags |= HV_FLUSH_NON_GLOBAL_MAPPINGS_ONLY;
                 status = hv_do_rep_hypercall(
                         HVCALL_FLUSH_VIRTUAL_ADDRESS_SPACE_EX,
